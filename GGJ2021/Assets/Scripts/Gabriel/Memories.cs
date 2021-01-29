c083627f20c774db6cb23ad37af7e9a6365ad607
Gabriel Marcoccia <gabrielmarcoccia36@gmail.com>
2021-01-29 18:06:07 -0500

    Achievements and memories progress

--- GGJ2021/Assets/Scripts/Gabriel/Memories.cs
+++ GGJ2021/Assets/Scripts/Gabriel/Memories.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Memories : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Achievements achievements;
+
+    public int memoriesToWin = 6;
+
+    private bool[] unlockedMemory = { false, false, false, false, false, false, false, false, false, false };
+    private bool looking = false;
+    private int curMemory, progress;
+
+    private void Awake()
     {
-        
+        achievements = FindObjectOfType<Achievements>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMemoryCollect(int id)
     {
-        
+        if (!looking && !unlockedMemory[id])
+        {
+            looking = true;
+            curMemory = id;
+        }
+        else
+        {
+            if (id == curMemory)
+            {
+                looking = false;
+                unlockedMemory[id] = true;
+                progress++;
+                achievements.GotMemory(id);
+            }
+            else
+            {
+                // Hint stuff goes here
+            }
+        }
+
+        if (progress >= memoriesToWin)
+        {
+            // Call win function here
+        }
     }
 }
