42543c53d359ce76db3a5e0524553cd3af3401fc
Gabriel Marcoccia <gabrielmarcoccia36@gmail.com>
2021-01-28 21:18:16 -0500

    Progress of player movement

---
+++ GGJ2021/Assets/Scripts/Gabriel/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Fields
+
+    public float speed = 2.5f;
+    private Rigidbody2D rb;
+    Vector2 movement;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.SqrMagnitude());
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
