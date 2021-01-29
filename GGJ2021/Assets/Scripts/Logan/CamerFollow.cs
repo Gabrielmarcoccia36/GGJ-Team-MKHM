d21146e67d5414fdc8ff85c47380f196b1d59632
Logan Stigler <loganbusiness546@gmail.com>
2021-01-28 20:16:34 -0800

    camera stuff

---
+++ GGJ2021/Assets/Scripts/Logan/CamerFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerFollow : MonoBehaviour
+{
+    public Vector2[] CameraPosition;
+
+    public void EnterTrigger(int room)
+    {
+           transform.position = CameraPosition[room];
+       
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = CameraPosition[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GGJ2021/Assets/Scripts/Logan/RoomTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public int RoomNumber;
+    public Rigidbody2D rb;
+    private CamerFollow camerFollow;
+
+    void Awake()
+    {
+        camerFollow = FindObjectOfType<CamerFollow>();
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<CharacterController>())
+        {
+         camerFollow.EnterTrigger(RoomNumber);
+
+        }
+    }
+ 
+}
