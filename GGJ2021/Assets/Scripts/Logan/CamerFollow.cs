5219fbea98989657c59626d51a858a26a0edc061
Logan Stigler <loganbusiness546@gmail.com>
2021-01-31 13:26:49 -0800

    Camera script

--- GGJ2021/Assets/Scripts/Logan/CamerFollow.cs
+++ GGJ2021/Assets/Scripts/Logan/CamerFollow.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class CamerFollow : MonoBehaviour
 {
     public Vector3[] CameraPosition;
+    public Vector3[] MemoryPosition;
+    public GameObject memoryCanvas;
 
     public void EnterTrigger(int room)
     {
            transform.position = CameraPosition[room];
+        memoryCanvas.transform.position = MemoryPosition[room];
     }
 
     void Start()
     {
+        MemoryPosition = CameraPosition;
+        for (int i = 0; i < MemoryPosition.Length; i++)
+        {
+            MemoryPosition[i].z = -5;
+        } 
         transform.position = CameraPosition[0];
+        memoryCanvas.transform.position = MemoryPosition[0];
     }
 }
